1a64eb6fd020eb5120e5d2cea7ac058234ff83ab
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-12 19:38:22 +0200

    Add files via upload

---
+++ Inheritance - Lab/01. Single Inheritance/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
