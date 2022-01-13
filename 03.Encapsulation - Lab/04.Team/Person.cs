e57dc3bc6337582b52857d0dfa129ae4729eb951
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 11:26:47 +0200

    Add files via upload

---
+++ 03.Encapsulation - Lab/04.Team/Person.cs
@@ -0,0 +1,97 @@
+﻿using System;
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        private const string invalidFirstName = "First name cannot contain fewer than 3 symbols!";
+        private const string invalidLastName = "Last name cannot contain fewer than 3 symbols!";
+        private const string invalidAge = "Age cannot be zero or a negative integer!";
+        private const string invalidSalary = "Salary cannot be less than 650 leva!";
+
+        private const int numSymbols = 3;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName {
+            get => firstName;
+            private set
+            {
+                if(value.Length < numSymbols)
+                {
+                    throw new ArgumentException(invalidFirstName);
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName {
+            get => lastName;
+            private set
+            {
+                if(value.Length < numSymbols)
+                {
+                    throw new ArgumentException(invalidLastName);
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age {
+            get => age;
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException(invalidAge);
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => salary;
+            private set
+            {
+                if(value < 460)
+                {
+                    throw new ArgumentException(invalidSalary);
+                }
+
+                salary = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if(Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+        }
+    }
+}
---
+++ 03.Encapsulation - Lab/04.Team/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Person> persons = new List<Person>();
+
+            var lines = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < lines; i++)
+            {
+                var arg = Console.ReadLine().Split(" ").ToArray();
+
+                var person = GetPerson(arg);
+                persons.Add(person);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach(Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+
+        private static Person GetPerson(string[] arg)
+        {
+            string firstName = arg[0];
+            string lastName = arg[1];
+            int age = int.Parse(arg[2]);
+            decimal salary = decimal.Parse(arg[3]);
+
+            Person person = new Person(firstName, lastName, age, salary);
+            return person;
+        }
+    }
+}
