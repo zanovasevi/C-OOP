a5abb59bc89726d402210d8cf089662cc69f4e59
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 11:48:30 +0200

    Create StartUp.cs

---
+++ 09.Reflection and Attributes - Exercise/01. Command Pattern/StartUp.cs
@@ -0,0 +1,16 @@
+using System;
+using CommandPattern.Core;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ICommandInterpreter commandInterpreter = new CommandInterpreter();
+            IEngine engine = new Engine(commandInterpreter);
+            engine.Run();
+        }
+    }
+}
