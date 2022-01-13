4694c36bc49db157f619a13d123f748110d4bf8b
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 11:52:40 +0200

    Add files via upload

---
+++ 10.Unit Testing - Lab/02. Dummy Tests/DummyTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class DummyTests
+{
+    [Test]
+    public void DummyShouldLoseHealthAttack()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(100, 100);
+
+        //Act
+        dummy.TakeAttack(10);
+
+        //Assert
+        var expectedResult = 90;
+        var actualResult = dummy.Health;
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void DeadDummyShouldThrowExceptionIfAttacked()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 100);
+
+        //Act Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(10));
+    }
+
+    [Test]
+    public void DeadDummyShouldGiveXp()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 100);
+
+        //Act - Assert
+        var expectedResult = 100;
+        var actualResult = dummy.GiveExperience();
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void AliveDummyShouldNotdGiveXp()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(100, 100);
+
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+    }
+}
