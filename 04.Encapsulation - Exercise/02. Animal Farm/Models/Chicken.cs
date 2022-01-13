d85441f97404556c0bebc4b83c421dcae694c998
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 11:29:35 +0200

    Add files via upload

---
+++ 04.Encapsulation - Exercise/02. Animal Farm/Models/Chicken.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int minAge = 0;
+        private const int maxAge = 15;
+
+        private string name;
+        private int age;
+
+        internal Chicken(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if(string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if(value < minAge || value > maxAge)
+                {
+                    throw new ArgumentException($"Age should be between {minAge} and {maxAge}.");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+			get
+			{				
+				return this.CalculateProductPerDay();
+			}
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
